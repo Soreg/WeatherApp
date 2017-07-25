16e4557360da0ebc06ffa02672fee7e5ed37af5b
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-07-25 13:13:35 +0200

    Script: Removed leftover code from old functions

--- script.js
+++ script.js
@@ -67,69 +67,6 @@ var skycons = new Skycons({"color": "white"});
 
 skycons.play();
     });
-
-
-
-
-    /*$.getJSON(api, function(data) {
-      var fTemp;
-      var cTemp;
-      var kTemp;
-      var tempSwap = true;
-      //List needed data from api
-      var weatherType = data.weather[0].description;
-      kTemp = data.main.temp;
-      var city = data.name;
-      var country = data.sys.country;
-
-      //Temperature in Fahrenheit
-      fTemp = (kTemp * (9 / 5) - 459.67).toFixed(1);
-      //temperature in Celcius
-      cTemp = (kTemp - 273).toFixed(1);
-
-      //Assign html elements
-      $("#location").html("Local weather in: " + city + ", " + country);
-      $("#weatherType").html(weatherType);
-      $("#degree").html(cTemp + " &#8451");
-
-
-      $("#degree").click(function() {
-        if (tempSwap == false) {
-          $("#degree").html(cTemp + " &#8451");
-          tempSwap = true;
-        } else {
-          $("#degree").html(fTemp + " &#8457");
-          tempSwap = false;
-        }
-      });
-
-      //If statements for image -- Weathertype and time
-      //Clear sky, day
-      if (weatherType == 'clear sky') {
-        $("#weatherState").attr("class", "wi wi-day-sunny");
-      } else if (weatherType == 'scattered clouds' || weatherType == 'broken clouds') {
-        $("#weatherState").attr("class", "wi wi-cloudy");
-      } //Few clouds, day
-      else if (weatherType == 'few clouds') {
-        $("#weatherState").attr("class", "wi wi-cloudy");
-      } //Rain, day
-      else if (weatherType == 'light intensity shower rain' || weatherType == 'shower rain' || weatherType == 'rain') {
-        $("#weatherState").attr("class", "wi wi-rain");
-      } //Thunderstorm, day & night
-      else if (weatherType == 'thunderstorm') {
-        $("#weatherState").attr("class", "wi wi-thunderstorm");
-      } //Snow, day & night
-      else if (weatherType == 'snow') {
-        $("#weatherState").attr("class", "wi wi-snow");
-      } //else -- mist, day & night
-      else {
-        $("#cloweatherStateuds").attr("class", "wi wi-fog");
-      }
-
-
-
-    });*/
-
   });
 
 });
