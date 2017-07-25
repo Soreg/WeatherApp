932f71c2139ad7c94fcca4db7e77f64323a895a4
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-07-25 13:11:16 +0200

    Weather functions: Added visual representation (forecast)

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   let lat;
   let long;
 
+// Add skycons
+var skycons = new Skycons({"color": "white"});
+
   $.getJSON('http://ip-api.com/json', function(data2) {
     lat = data2.lat;
     long = data2.lon;
@@ -9,15 +12,15 @@ $(document).ready(function() {
     let city = data2.city;
     // Get weather api
     var api = 'https://api.darksky.net/forecast/b1771da80a45a69cc2fcaf3cdbe9ab1a/' + lat + ',' + long + "?&units=auto";
-
     // Get weather data through JSON
     $.getJSON(api, function(data) {
 
       // Assign data
       let timezone = data.timezone;
       let currentWeather = data.currently.summary;
-      var temp = data.currently.temperature;
+      let temp = data.currently.temperature;
       let tempCalc = temp;
+      let weatherType = data.currently.icon;
       (data.flags.units === 'si') ? tempState = 1 : tempState = 2;
       (tempState === 1) ? temp = temp + "101"+String.fromCharCode(176) +"C" : temp = temp + "101"+String.fromCharCode(176) +"F" ;
 
@@ -25,13 +28,11 @@ $(document).ready(function() {
         if(tempState === 1) {
           tempCalc = (tempCalc * 9 / 5 + 32);
           temp = tempCalc.toFixed(2) + "101"+String.fromCharCode(176) +"F";
-          console.log(tempCalc);
           $('#degree').html(temp);
           tempState = 2;
         } else {
           tempCalc = ((5/9) * (tempCalc-32));
           temp = tempCalc.toFixed(2) + "101"+String.fromCharCode(176) +"C";
-          console.log(tempCalc);
           $('#degree').html(temp);
           tempState = 1;
         }
@@ -40,9 +41,31 @@ $(document).ready(function() {
       $('#location').html('Current weather in: ' + city + ", " + country);
       $('#weatherType').html(currentWeather);
       $('#degree').html(temp);
+      if(weatherType === 'partly-cloudy-day') {
+        skycons.add('icon1', Skycons.PARTLY_CLOUDY_DAY);
+      } else if(weatherType === 'clear-day') {
+        skycons.add('icon1', Skycons.CLEAR_DAY);
+      } else if(weatherType === 'clear-night') {
+        skycons.add('icon1', Skycons.CLEAR_NIGHT);
+      } else if(weatherType === 'rain') {
+        skycons.add('icon1', Skycons.RAIN);
+      } else if(weatherType === 'snow') {
+        skycons.add('icon1', Skycons.SNOW);
+      } else if(weatherType === 'sleet') {
+        skycons.add('icon1', Skycons.SLEET);
+      } else if(weatherType === 'wind') {
+        skycons.add('icon1', Skycons.WIND);
+      } else if(weatherType === 'fog') {
+        skycons.add('icon1', Skycons.FOG);
+      } else if(weatherType === 'cloudy') {
+        skycons.add('icon1', Skycons.CLOUDY);
+      } else if(weatherType === 'partly-cloudy-day') {
+        skycons.add('icon1', Skycons.PARTLY_CLOUDY_DAY);
+      } else if(weatherType === 'partly-cloudy-night') {
+        skycons.add('icon1', Skycons.PARTLY_CLOUDY_NIGHT);
+      }
 
-
-
+skycons.play();
     });
 
 
